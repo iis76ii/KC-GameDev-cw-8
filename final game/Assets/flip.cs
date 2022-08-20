9f612211980d1de361ac13b05c2a147f79b86796
IIS76II <108532889+IIS76II@users.noreply.github.com>
2022-08-20 14:43:59 +0300

    cw8

---
+++ final game/Assets/flip.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flip : MonoBehaviour
+{
+    SpriteRenderer sprite;
+    bool faceright = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipPlayer();
+    }
+    void FlipPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.D) && faceright == false)
+        {
+            sprite.flipX = false;
+            faceright = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) && faceright == true)
+        {
+            sprite.flipX = true;
+            faceright = false;
+        }
+    }
+}
